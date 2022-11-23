f3fc72baa9f05c973641781dfba396d03b252a10
aymeric s <sandoz.aymeric@gmail.com>
2022-11-23 15:03:08 +0100

    config

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "my-app/build")));
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/my-app/build/index.html"));
 });
 
 app.post("/jwtid", requireAuth, (req, res) => {
