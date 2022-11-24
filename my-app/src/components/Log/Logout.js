80b60b354cbd99fc07ca738f2c74782fe45052f3
aymeric s <sandoz.aymeric@gmail.com>
2022-11-24 05:00:14 +0100

    config

--- my-app/src/components/Log/Logout.js
+++ my-app/src/components/Log/Logout.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { FaUsersSlash } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 const Logout = () => {
+  const navigate = useNavigate();
   const clearToken = () => {
     if (window.confirm("Voulez-vous vous déconnectez ?")) {
       localStorage.clear();
+
+      navigate("/log");
       window.location = "/log";
     }
   };
